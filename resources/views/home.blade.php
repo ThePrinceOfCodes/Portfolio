b0c56f2db4fe90e0a5cc79633395496db873c712
theprinceofcodes <prince@underthehammer.com>
2023-01-10 11:45:16 +0100

    video tutorial components completed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,4 +3,5 @@
     <x-home.learn-php></x-learn-php>
     <x-home.about></x-home.about>
     <x-home.portfolio></x-home.portfolio>
+    <x-home.video-tutorials></x-home.video-tutorials>
 </x-app-layout>
